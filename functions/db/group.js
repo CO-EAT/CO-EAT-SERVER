b9bdabdd35b070b32e6f0cd66ac2246ad25e9fff
eojinsarang joo <wndjwlstkfkd@naver.com>
2021-12-18 17:55:48 +0900

    feat: 코잇 종료하기

--- functions/db/group.js
+++ functions/db/group.js
@@ -26,4 +26,28 @@ const addGroup = async (client, hostName, inviteCode) => {
   return convertSnakeToCamel.keysToCamel(rows);
 };
 
-module.exports = { findGroupByInviteCode, addGroup };
+const checkHost = async (client, groupId, nickname)=>{
+  const { rows } = await client.query(
+    `
+    SELECT * from "group" g
+    WHERE id = $1 and host_name = $2
+    `,
+    [groupId, nickname],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+const completeGroup = async (client, groupId, nickname)=>{
+  const { rows } = await client.query(
+    `
+    UPDATE "group" g
+    SET is_deleted = TRUE, updated_at = now()
+    WHERE id = $1 and host_name = $2
+    RETURNING is_deleted
+    `,
+    [groupId, nickname],
+  );
+  return convertSnakeToCamel.keysToCamel(rows[0]);
+};
+
+module.exports = { findGroupByInviteCode, addGroup, checkHost, completeGroup };
