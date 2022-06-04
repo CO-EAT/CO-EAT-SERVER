ef2405b59df8461789060b607c018a53c6101439
kanghanhee <kanghanhee59@gmail.com>
2022-06-04 23:19:25 +0900

    #46 fix: 음식 정렬

--- functions/db/menu.js
+++ functions/db/menu.js
@@ -4,7 +4,7 @@ const convertSnakeToCamel = require('../lib/convertSnakeToCamel');
 const findAllMenu = async (client) => {
   const { rows } = await client.query(
     `
-    SELECT * FROM menu
+    SELECT * FROM menu ORDER BY desc
     `,
   );
   return convertSnakeToCamel.keysToCamel(rows);
